85c0fa12f95ea1d5cde4b49425f45c587492220c
Alan Wan <alan.wan@oocl.com>
2022-12-19 16:51:05 +0800

    [35] feat: retrieved selected movie from db and saved to store

--- src/features/DisplayMovie.js
+++ src/features/DisplayMovie.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 
 const DisplayMovie = () => {
+    const movie = useSelector((state) => {
+        return state.movie.selectedMovie;
+    })
     return (
         <>
             <div>
                 Movie
+                <span> {movie.movieName} </span>
             </div>
         </>
     )
--- src/pages/CinemaListPage.js
+++ src/pages/CinemaListPage.js
@@ -1,12 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getCinemas } from "../api/cinemas";
 import  DisplayMovie  from "../features/DisplayMovie";
 import  DisplyaCinemas  from "../features/DisplayCinemas";
+import { getMovieById } from "../api/movies";
+import { addSelectedMovie } from "../features/movieSlice";
+import { useDispatch } from "react-redux";
 
 
 export default function CinemaListPage() {
+    const dispatch = useDispatch();
+    const [movieId, setMovieId] = useState("63a0100399e65b544dc8e14a");
 
     useEffect(() => {
+        getMovieById(movieId).then((response) => {
+            console.log(response.data);
+            dispatch(addSelectedMovie(response.data));
+        })
         getCinemas().then((response) => {
             console.log(response.data);
         })

ee77ad16d84d0059cc35151454254696720af823
kwokth4 <118238880+kwokth4@users.noreply.github.com>
2022-12-19 20:30:11 +0800

    feat: show sessions

--- src/features/movieSession/SessionLists.js
+++ src/features/movieSession/SessionLists.js
@@ -1,20 +1,25 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getSessions } from "../../api/session";
+import Session from "./Session";
 
 function SessionLists() {
   const movie = useSelector((state) => {
     return state.movie.selectedMovie;
   });
-  const sessions = [];
+  const [sessions, setSessions] = useState([]);
   useEffect(() => {
-    getSessions(/*movieId*/"63a03d05a8c97353fa8cb52f", "63a03cdca8c97353fa8cb52e" /*cinemaId*/).then((response) => {
+    getSessions(
+      /*movieId*/ "63a03d05a8c97353fa8cb52f",
+      /*cinemaId*/ "63a03cdca8c97353fa8cb52e"
+    ).then((response) => {
       console.log(response.data);
-    //   sessions= response.data;
+      setSessions(response.data);
     });
+  }, []);
+  return sessions.map((session) => {
+    return <Session session={session}/>
   });
-  return <div>SessionLists page</div>;
 }
 
 export default SessionLists;

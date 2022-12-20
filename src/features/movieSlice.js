1592f8bacb1f7da1908d9605465f7290a859e4e6
Marie Chow <chowma3@oocl.com>
2022-12-20 11:17:43 +0800

    [18] refactor: remove movies in store

--- src/features/movieSlice.js
+++ src/features/movieSlice.js
@@ -1,59 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initMovies = [
-  {
-    movieId: "63a00c9e8c78635f83e882f4",
-    movieName: "Advengers 1",
-    description: "action movie",
-    duration: 116,
-    movieSessionIds: ["63a010108c78635f83e882f7"],
-    lastShowDateTime: "2022-12-22T03:45:00",
-    thumbnailUrl:
-      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-  },
-  {
-    movieId: "63a026788c78635f83e882f9",
-    movieName: "Advengers 2",
-    description: "another action movie",
-    duration: 116,
-    movieSessionIds: ["63a02c498c78635f83e882fb"],
-    lastShowDateTime: "2022-12-22T03:45:00",
-    thumbnailUrl:
-      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-  },
-  {
-    movieId: "63a00c9e8c78635f83e882f5",
-    movieName: "Advengers 3",
-    description: "action movie",
-    duration: 116,
-    movieSessionIds: ["63a010108c78635f83e882f7"],
-    lastShowDateTime: "2022-12-22T03:45:00",
-    thumbnailUrl:
-      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-  },
-  {
-    movieId: "63a026788c78635f83e882f8",
-    movieName: "Advengers 4",
-    description: "another action movie",
-    duration: 116,
-    movieSessionIds: ["63a02c498c78635f83e882fb"],
-    lastShowDateTime: "2022-12-22T03:45:00",
-    thumbnailUrl:
-      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-  },
-];
 export const movieSlice = createSlice({
   name: "movie",
-  initialState: {
-    movies: initMovies,
-  },
-  reducers: {
-    addMovies: (state, action) => {
-      state.movies = action.payload;
-    },
-  },
+  initialState: {},
+  reducers: {},
 });
-
-export const { addMovies } = movieSlice.actions;
 
 export default movieSlice.reducer;

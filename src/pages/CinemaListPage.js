d360416d80a8b0d981e0d75a127c9396b963b4b0
Marie Chow <chowma3@oocl.com>
2022-12-20 15:08:53 +0800

    [39] feat: connect movie to cinema page

--- src/pages/CinemaListPage.js
+++ src/pages/CinemaListPage.js
@@ -1,23 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import DisplayMovie from "../features/DisplayMovie";
 import DisplyaCinemas from "../features/DisplayCinemas";
-import { addCinemas, addSelectedMovie } from "../features/movieSlice";
+import { addCinemas } from "../features/movieSlice";
 import { useDispatch } from "react-redux";
 import { Divider } from "antd";
 import { Layout } from "antd";
-import { getCinemas, getMovieById } from "../api/movies";
+import { getCinemas } from "../api/movies";
 
 const { Footer, Content } = Layout;
 
 export default function CinemaListPage() {
   const dispatch = useDispatch();
-  const [movieId] = useState("63a03d05a8c97353fa8cb52f");
-
   useEffect(() => {
-    getMovieById(movieId).then((response) => {
-      console.log(response.data);
-      dispatch(addSelectedMovie(response.data));
-    });
     getCinemas().then((response) => {
       console.log(response.data);
       dispatch(addCinemas(response.data));

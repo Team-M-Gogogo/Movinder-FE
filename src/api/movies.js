5ba039d2914020188d06f4893258bff1bdecd535
Marie Chow <chowma3@oocl.com>
2022-12-20 14:43:31 +0800

    refactor: merge apis to 1 file

--- src/api/movies.js
+++ src/api/movies.js
@@ -12,3 +12,19 @@ export const getMovies = () => {
 export const getMovieById = (movieId) => {
   return api.get("/movie/films/" + movieId);
 };
+
+export const getCinemas = () => {
+  return api.get("/movie/cinemas");
+};
+
+export const getCinemasById = (cinemaId) => {
+  return api.get("/movie/cinemas" + cinemaId);
+};
+
+export const getCinemasByMovieId = (cinemaId) => {
+  return api.get("/movie/cinemas" + cinemaId);
+};
+
+export const getSessions = (movieId, cinemaId) => {
+  return api.get("/movie/sessions?filmID=" + movieId + "&cinemaID=" + cinemaId);
+};
--- src/pages/CinemaListPage.js
+++ src/pages/CinemaListPage.js
@@ -1,47 +1,41 @@
-import { useEffect, useState } from "react"
-import { getCinemas } from "../api/cinemas";
-import  DisplayMovie  from "../features/DisplayMovie";
-import  DisplyaCinemas  from "../features/DisplayCinemas";
-import { getMovieById } from "../api/movies";
+import { useEffect, useState } from "react";
+import DisplayMovie from "../features/DisplayMovie";
+import DisplyaCinemas from "../features/DisplayCinemas";
 import { addCinemas, addSelectedMovie } from "../features/movieSlice";
 import { useDispatch } from "react-redux";
-import { Divider} from 'antd';
-import { Layout} from "antd";
+import { Divider } from "antd";
+import { Layout } from "antd";
+import { getCinemas, getMovieById } from "../api/movies";
 
 const { Footer, Content } = Layout;
 
-
 export default function CinemaListPage() {
-    const dispatch = useDispatch();
-    const [movieId] = useState("63a03d05a8c97353fa8cb52f");
+  const dispatch = useDispatch();
+  const [movieId] = useState("63a03d05a8c97353fa8cb52f");
 
-    useEffect(() => {
-        getMovieById(movieId).then((response) => {
-            console.log(response.data);
-            dispatch(addSelectedMovie(response.data));
-        })
-        getCinemas().then((response) => {
-            console.log(response.data);
-        })
-        getCinemas().then((response) => {
-            console.log(response.data);
-            dispatch(addCinemas(response.data));
-        })
-    })
+  useEffect(() => {
+    getMovieById(movieId).then((response) => {
+      console.log(response.data);
+      dispatch(addSelectedMovie(response.data));
+    });
+    getCinemas().then((response) => {
+      console.log(response.data);
+      dispatch(addCinemas(response.data));
+    });
+  });
 
-
-    return (
-        <div>
-            <Layout style={{ padding: "0 50px"}}>
-                <Content className="site-layout" style={{ padding: "0 50px"}}>
-                    <DisplayMovie/>
-                    <Divider></Divider>
-                    <DisplyaCinemas/>
-                    <Footer style={{ textAlign: "center" }}>
-                        Team M Gogogo@2022 No time to die
-                    </Footer>
-                </Content>
-            </Layout>
-        </div>
-    )
+  return (
+    <div>
+      <Layout style={{ padding: "0 50px" }}>
+        <Content className="site-layout" style={{ padding: "0 50px" }}>
+          <DisplayMovie />
+          <Divider></Divider>
+          <DisplyaCinemas />
+          <Footer style={{ textAlign: "center" }}>
+            Team M Gogogo@2022 No time to die
+          </Footer>
+        </Content>
+      </Layout>
+    </div>
+  );
 }

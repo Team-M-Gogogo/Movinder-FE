1887ef21fa0754231313470de771ed2558fd65e4
TAMVI <mansantamm@gmail.com>
2022-12-21 17:16:12 +0800

    [37] feat natbar-fix-2

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from '../features/Navbar'
 import "../features/Navbar/Navbar.css";
+import { Layout as layout }  from "antd";
+
+const { Footer } = layout;
 
 export default function Layout() {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer style={{ textAlign: "center" }}>
+                Team M Gogogo@2022 No time to die
+      </Footer>
     </div>
   );
 }

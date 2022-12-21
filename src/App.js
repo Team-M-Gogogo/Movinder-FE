13ab1cceec317764fb6a662b278373c6a24fd7ee
Marie Chow <chowma3@oocl.com>
2022-12-21 15:08:21 +0800

    [17] refator: auto style App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import Layout from "./layout/Layout";
 import HomePage from "./pages/HomePage";
 import CinemaListpage from "./pages/CinemaListPage";
 import LoginPage from "./pages/LoginPage";
-import {Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import BookingPage from "./pages/BookingPage";
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<HomePage/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
           {/* <Route path="/cinema" element={<CinemaListpage/>}/> */}
-          <Route path="/:movieId" element={<CinemaListpage/>}/>
-          <Route path="/:movieId/:cinemaId/:sessionId" element={<BookingPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/:movieId" element={<CinemaListpage />} />
+          <Route
+            path="/:movieId/:cinemaId/:sessionId"
+            element={<BookingPage />}
+          />
+          <Route path="/login" element={<LoginPage />} />
         </Route>
       </Routes>
-
-
     </div>
   );
 }

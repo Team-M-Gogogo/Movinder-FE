4da0b915882c1ac8be711b1a1bddcc2e3d7f0719
WONGCH9 <wongch9@oocl.com>
2022-12-21 11:21:22 +0800

    [22] payment page

---
+++ src/features/payment/Payment.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Payment.css';
+import {  Image } from "antd";
+
+export default function Payment(props) {
+    // const movie = props.selectedMovie;
+
+    return (
+        <div className="centered-div">
+        <h2>Buy Ticket</h2>
+        <Image
+                // src={movie.thumbnailUrl}
+        />
+        
+        </div>
+
+    )
+}

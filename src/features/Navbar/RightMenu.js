a31a6e4e7f3c3262a13a23bbea473dda22c54e7c
TAMVI <mansantamm@gmail.com>
2022-12-21 17:11:54 +0800

    [37] Feat: update Navbar - bugfix

--- src/features/Navbar/RightMenu.js
+++ src/features/Navbar/RightMenu.js
@@ -5,8 +5,10 @@ import User from "./image/user.png";
 import { useState } from "react";
 import { Modal, Button } from "antd";
 import { postLogin } from "../../api/movies";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const RightMenu = () => {
+  const navigate = useNavigate();
   const localUser = localStorage.getItem("User");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const user =
@@ -60,7 +62,8 @@ const RightMenu = () => {
     localStorage.setItem("User", "");
     success_logout();
   };
-
+  const location = useLocation();
+  console.log("ff", location);
   const login = () => {
     const loginInfo = { username: username, password: password };
     postLogin(loginInfo)
@@ -68,6 +71,7 @@ const RightMenu = () => {
         localStorage.setItem("User", JSON.stringify(response.data));
         console.log(localStorage.getItem("User"));
         success();
+        navigate(location.pathname);
       })
       .catch((error) => {
         console.error(error);
@@ -77,67 +81,64 @@ const RightMenu = () => {
     setUserName("");
     setPassword("");
   };
+  const loginNav = (<><Button onClick={showModal}>
+    {user === "" ? "Login" : user.customerName + " Logout"}
+  </Button>
+
+  <Modal
+    title=""
+    open={isModalOpen}
+    onOk={handleOk}
+    onCancel={handleCancel}
+  >
+    {user === "" ? (
+      <div className="login-wrapper">
+        <h1>Log In</h1>
+        <label>
+          <p>Username</p>
+          <input
+            type="text"
+            value={username || ""}
+            onChange={onUsernameChange}
+          />
+        </label>
+        <br />
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            value={password || ""}
+            onChange={onPasswordChange}
+          />
+        </label>
+      </div>
+    ) : (
+      <h1>
+        Hi {user === "" ? "" : user.customerName}, Are you sure to logout?{" "}
+      </h1>
+    )}
+  </Modal></>)
+
+  const items=[
+    {
+      label: <a href="/"><img src={setting} style={{ width: "20%" }} alt="Logo" />Home</a>,
+      key: 'Home',
+    },
+    {
+      label: ( user && <a href="/userprofile"><img src={User} style={{ width: "20%" }} alt="Logo" />User</a>),
+      key: 'User',
+    },
+    {
+      label: loginNav,
+      key: 'login',
+    },
+
+  ] 
+
+
 
   return (
-    <Menu mode="horizontal">
-      <Menu.Item>
-        <a href="/">
-          <img src={setting} style={{ width: "20%" }} alt="Logo" />
-          Home
-        </a>
-      </Menu.Item>
-
-      {user === "" ? (
-        ""
-      ) : (
-        <Menu.Item>
-          <a href="/userprofile">
-            <img src={User} style={{ width: "20%" }} alt="Logo" />
-            User
-          </a>
-        </Menu.Item>
-      )}
-
-      <Menu.Item>
-        <Button onClick={showModal}>
-          {user === "" ? "Login" : user.customerName + " Logout"}
-        </Button>
-
-        <Modal
-          title=""
-          open={isModalOpen}
-          onOk={handleOk}
-          onCancel={handleCancel}
-        >
-          {user === "" ? (
-            <div className="login-wrapper">
-              <h1>Log In</h1>
-              <label>
-                <p>Username</p>
-                <input
-                  type="text"
-                  value={username || ""}
-                  onChange={onUsernameChange}
-                />
-              </label>
-              <br />
-              <label>
-                <p>Password</p>
-                <input
-                  type="password"
-                  value={password || ""}
-                  onChange={onPasswordChange}
-                />
-              </label>
-            </div>
-          ) : (
-            <h1>
-              Hi {user === "" ? "" : user.customerName}, Are you sure to logout?{" "}
-            </h1>
-          )}
-        </Modal>
-      </Menu.Item>
-    </Menu>
+    <Menu mode="horizontal" items={items}/>  
   );
 };
 

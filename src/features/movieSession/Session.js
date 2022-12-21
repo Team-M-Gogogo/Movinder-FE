4dd8523b1fc93171337ffd5ccd42ec037e4ed8d4
WONGJE4 <wongje4@oocl.com>
2022-12-21 20:05:16 +0800

    feat: tell the user to login before buying the ticket

--- src/features/movieSession/Session.js
+++ src/features/movieSession/Session.js
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { addSelectedCinema, addSelectedSession } from "../movieSlice";
@@ -9,10 +9,30 @@ function Session(props) {
   const { cinema, session } = props;
   const date = new Date(session.datetime);
   const dispatch = new useDispatch();
+  const localUser = localStorage.getItem("User");
+  const user =
+    localUser === null || localUser === ""
+      ? ""
+      : JSON.parse(localStorage.getItem("User"));
+  
+      const info = () => {
+        Modal.info({
+          title: 'Please login first before buying the ticket',
+          content: (
+            <div>
+              <p>You can click the top right login button on the navigation bar to login.</p>
+            </div>
+          ),
+          onOk() {},
+        });
+      };
+
   const handleClick = () => {
+    if (user){
     dispatch(addSelectedSession(session));
     dispatch(addSelectedCinema(cinema));
     navigate("/"+session.movieId+"/"+session.cinemaId+"/"+session.sessionId);
+    }else{info();}
 
   };
   return (

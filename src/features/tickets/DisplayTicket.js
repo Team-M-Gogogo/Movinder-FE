d4bbc01dd14f01352573eb8b2049ac59c57808ae
Hezz <heinrich@connect.hku.hk>
2022-12-22 14:10:24 +0800

    fix: suppress warning

--- src/features/tickets/DisplayTicket.js
+++ src/features/tickets/DisplayTicket.js
@@ -69,7 +69,8 @@ export default function DisplayTickets(props) {
         }
       );
     });
-  });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const ticketCardGrid = tickets.map((ticket) => {
     return (
